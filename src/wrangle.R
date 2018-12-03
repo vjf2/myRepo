cbd1013e811d0e947279f74698f21fcc069faa42
Vivienne Foroughirad <foroughirad@gmail.com>
2018-12-03 13:57:00 -0500

    bind rows and rename

--- src/wrangle.R
+++ src/wrangle.R
@@ -1,3 +1,7 @@
 pdf(file="coolplot.pdf")
 hist(mtcars$mpg)
 dev.off()
+
+# Minimal Wrangling of the data
+gmpts <- bind_rows(gm182, gm182UP)
+colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'status')
