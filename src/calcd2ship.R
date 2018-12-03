816db6981c16a97a1e71bf555331d5f474a36d9a
Vivienne Foroughirad <foroughirad@gmail.com>
2018-12-03 14:04:01 -0500

    add calculate distance code

---
+++ src/calcd2ship.R
@@ -0,0 +1,8 @@
+# Analysis Section
+# Calculate distance to ship at the start of the CEE
+gmpts$d2ship <- rdist.earth.vec(cbind(kStart$longitude, kStart$latitude), 
+                                cbind(gmpts$longitude, gmpts$latitude))
+
+gmpts %>% 
+  group_by(status) %>% 
+  summarize(mean = mean(d2ship, na.rm = TRUE))
