1d22b8bcf53e6901a80aefc40c5c5b73ae3ec7d1
Vivienne Foroughirad <foroughirad@gmail.com>
2018-12-03 13:55:29 -0500

    add actual reading in code

--- src/readData.R
+++ src/readData.R
@@ -1,1 +1,28 @@
-something
+options(stringsAsFactors = FALSE)
+
+kahuna<-read.csv("data/2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv")
+
+CEE<-read.csv("data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv")
+
+locations<-read.csv("data/2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv")
+
+library(dplyr)
+library(readr)
+library(fields)
+library(lme4)
+library(ggplot2)
+
+# Read in data
+# positional data about the RV Kahuna
+# kahuna <- read_csv('../../../presentations/2018/2018-12-03_DUML-RepResearch/data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv') 
+kahuna <- read_csv('data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv')
+kStart <- kahuna %>% 
+  filter(status == 'start')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
+gm182UP <- read_csv('data/2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'userPoints')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182
+gm182 <- read_csv('data/2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'noUserPoints')
