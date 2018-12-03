7afc1f1c8a11f0dceebd5531c31b74e25673b9d3
Vivienne Foroughirad <foroughirad@gmail.com>
2018-12-03 13:59:29 -0500

    made plotData file

---
+++ src/plotData.R
@@ -0,0 +1,11 @@
+
+# Plot the points out:
+windows()
+ggplot(gmpts, aes(longitude, latitude, group = status))+
+  scale_fill_gradient(low = "grey70", high = "grey30", guide = "none") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_minimal()+
+  facet_grid(~ status, labeller = label_value) +
+  stat_density_2d(aes(fill = ..level..), geom = "polygon", color = "black", size = 0.2, alpha = 0.5) +
+  geom_point(color = "navy", size = .5, alpha = .4)
